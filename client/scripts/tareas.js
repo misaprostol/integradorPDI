95f834eab33a2d7c9ef3e4135a186f84324eb6e7
misaprostol <164538922+misaprostol@users.noreply.github.com>
2024-07-11 16:09:15 -0300

    completar funcion renderizarTareas

--- client/scripts/tareas.js
+++ client/scripts/tareas.js
@@ -14,8 +14,19 @@ const obtenerTareas = async () => {
   /**
    * Completa la lista <ul> con las tareas obtenidas
    */
-  const renderizarTareas = async () => {
-    // Su implementación
+    const renderizarTareas = async () => {
+    const tareas = await obtenerTareas();
+
+    const listaTareas = document.getElementById('lista-tareas')
+
+    listaTareas.innerHTML = '';
+
+    tareas.forEach(tarea => {
+      const li = document.createElementNS('li');
+      li.textContent = `${tarea.name}: ${tarea.description}`
+      listaTareas.appendChild(li)
+    });
+    }
   }
   
   renderizarTareas();
